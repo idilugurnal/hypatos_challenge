d4c44b1a93128e61a32ad8e57d55a0cdec85277b
İdil  Uğurnal <idilugurnal@Idils-MacBook-Pro.local>
2020-10-21 14:02:14 +0300

    Insert evaluation functions and output file creation

--- main.py
+++ main.py
@@ -48,6 +48,48 @@ def read_csv(filename):
     df = pd.read_csv(filename, usecols=["File_name", "Description", "Quantity", "Unit price", "Total price"], keep_default_na=False)
     return df
 
+def description_evaluation(description_gt, description_extraction):
+    return float("{:.2f}".format(calculate_distance(description_gt, description_extraction)/len(description_gt)))
+
+def numerical_evaluation(value_gt, value_extraction):
+    """
+    neden int çevirdiğini anlat. 60.01 olayını
+    """
+    if value_gt == "" or value_extraction == "": return ""
+    return int(not(convert_to_int(value_gt) == convert_to_int(value_extraction)))
+
+def create_output(extraction, gt, pairs):
+        """
+
+        """
+        output = gt.copy()
+        output.rename({"Description": "Description_GT","Quantity":"Quantity_GT", "Unit price":"Unit price_GT", "Total price":"Total price_GT" }, inplace=True, axis="columns")
+        output.insert(loc=2, column="Description_Extraction", value="")
+        output.insert(loc=4, column="Quantity_Extraction", value="")
+        output.insert(loc=6, column="Unit_price_Extraction", value="")
+        output.insert(loc=8, column="Total_price_Extraction", value="")
+
+        output.insert(loc=3, column="Evaluation", value="", allow_duplicates=True)
+        output.insert(loc=6, column="Evaluation", value="", allow_duplicates=True)
+        output.insert(loc=9, column="Evaluation", value="", allow_duplicates=True)
+        output.insert(loc=12, column="Evaluation", value="", allow_duplicates=True)
+        print()
+        for pair in pairs:
+            if pair[1] >= 0:
+                output.at[pair[0],"Description_Extraction"] = extraction["Description"][pair[1]]
+                output.iat[pair[0],3] = description_evaluation(gt["Description"][pair[0]], extraction["Description"][pair[1]])
+                output.at[pair[0],"Quantity_Extraction"] = extraction["Quantity"][pair[1]]
+                output.iat[pair[0],6] = numerical_evaluation(gt["Quantity"][pair[0]], extraction["Quantity"][pair[1]])
+                output.at[pair[0],"Unit_price_Extraction"] = extraction["Unit price"][pair[1]]
+                output.iat[pair[0],9] = numerical_evaluation(gt["Unit price"][pair[0]], extraction["Unit price"][pair[1]])
+                output.at[pair[0],"Total_price_Extraction"] = extraction["Total price"][pair[1]]
+                output.iat[pair[0],12] = numerical_evaluation(gt["Total price"][pair[0]], extraction["Total price"][pair[1]])
+
+        pd.set_option('display.max_columns' , None)
+        print("Output: ")
+        print(output)
+        return output
+
 
 if __name__ == "__main__":
 
@@ -60,3 +102,6 @@ if __name__ == "__main__":
 
     pairs = generate_matches(gt, extraction)
     print(pairs)
+
+    output = create_output(extraction, gt, pairs)
+    output.to_csv("output.csv", index=False ,float_format="{:.2f}")
